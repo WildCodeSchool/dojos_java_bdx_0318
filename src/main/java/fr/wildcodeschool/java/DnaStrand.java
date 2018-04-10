f5a7c6c192f9b054c99e7801236b7496aa19b60d
fabien <fabien.marsaud@wildcodeschool.fr>
2018-04-10 09:29:21 +0200

    DNA strand dojo

---
+++ src/main/java/fr/wildcodeschool/java/DnaStrand.java
@@ -0,0 +1,7 @@
+package fr.wildcodeschool.java;
+
+public class DnaStrand {
+    public static String makeComplement(String dna) {
+        //Your code
+    }
+}
---
+++ src/test/java/fr/wildcodeschool/java/DnaTest.java
@@ -0,0 +1,21 @@
+package fr.wildcodeschool.java;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DnaTest {
+    @Test
+    public void test01() {
+        assertEquals("TTTT", DnaStrand.makeComplement("AAAA"));
+    }
+    @Test
+    public void test02() {
+        assertEquals("TAACG", DnaStrand.makeComplement("ATTGC"));
+    }
+    @Test
+    public void test03() {
+        assertEquals("CATA", DnaStrand.makeComplement("GTAT"));
+    }
+}
